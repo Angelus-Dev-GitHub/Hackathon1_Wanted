0120fc66dc06af1da9772c1e4434c8348aa30181
AudeP <aude.plana@gmail.com>
2020-11-19 22:36:16 +0100

    back done

--- src/Model/CityManager.php
+++ src/Model/CityManager.php
@@ -19,4 +19,16 @@ class CityManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
+    public function addCityForWanted($city, $id)
+    {
+        $query = "INSERT INTO wanted_city (city_id, wanted_id, created_at) 
+                    VALUES (:city_id, :wanted_id, created_at)";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':city_id', $city['city_id'], \PDO::PARAM_INT);
+        $statement->bindValue(':wanted_id', $id, \PDO::PARAM_INT);
+        $statement->bindValue(':created_at', $city['created_at'], \PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
+    
 }
